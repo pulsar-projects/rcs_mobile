07c130d7335bd56c47efe51cf92306e2179aac40
gabi spanoae <4469615+gspanoae@users.noreply.github.com>
2020-06-28 23:27:18 +0100

    gh-14: enable Find nearest recycle centers

--- lib/screens/recycle_item_add_screen.dart
+++ lib/screens/recycle_item_add_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:rcs_mobile/common/tflite_helper.dart';
+import 'package:rcs_mobile/screens/recycle_centers.dart';
 import 'package:rcs_mobile/widgets/common/index.dart';
 
 class RecycleItemAddScreen extends StatefulWidget {
@@ -60,48 +61,48 @@ class _RecycleItemAddScreenState extends State<RecycleItemAddScreen> {
       appBar: AppBar(),
       body: (_isLoading == true)
           ? Center(
-        child: CircularProgressIndicator(),
-      )
+              child: CircularProgressIndicator(),
+            )
           : ListView(
-        children: <Widget>[
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.6,
-            child: ProcessedImage(
-              imgFile: _predictedImage,
-              renderBoxes: _renderBoxes,
-              size: size,
+              children: <Widget>[
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  child: ProcessedImage(
+                    imgFile: _predictedImage,
+                    renderBoxes: _renderBoxes,
+                    size: size,
+                  ),
+                ),
+                RaisedButton(
+                  child: Text('Find nearest recycle centers'),
+                  textColor: Colors.white,
+                  onPressed: (_recognitions.length == 0)
+                      ? null
+                      : () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RecycleCentersMap(
+                                  recognitions: _recognitions),
+                            ),
+                          );
+                        },
+                  color: Colors.green,
+                ),
+                RaisedButton(
+                  child: Text('Load image from device'),
+                  onPressed: selectFromImagePicker,
+                  textColor: Colors.white,
+                  color: Colors.green,
+                ),
+                RaisedButton(
+                  child: Text('Take photo'),
+                  onPressed: null,
+                  textColor: Colors.white,
+                  color: Colors.green,
+                )
+              ],
             ),
-          ),
-          RaisedButton(
-            child: Text('Find nearest recycle centers'),
-            textColor: Colors.white,
-//            onPressed: (_recognitions.length == 0)
-//                ? null
-//                : () {
-//                    Navigator.push(
-//                      context,
-//                      MaterialPageRoute(
-//                        builder: (context) =>
-//                            RecycleCentersMap(recognitions: _recognitions),
-//                      ),
-//                    );
-//                  },
-            color: Colors.green,
-          ),
-          RaisedButton(
-            child: Text('Load image from device'),
-            onPressed: selectFromImagePicker,
-            textColor: Colors.white,
-            color: Colors.green,
-          ),
-          RaisedButton(
-            child: Text('Take photo'),
-            onPressed: null,
-            textColor: Colors.white,
-            color: Colors.green,
-          )
-        ],
-      ),
     );
   }
 }
